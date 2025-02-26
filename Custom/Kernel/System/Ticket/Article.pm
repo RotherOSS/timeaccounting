15cbce6c9acac2ca1ee5232c144933a244582312
Sven <svo@otobo.org>
2025-02-26 11:38:43 +0100

    Update OTOBO (nonfunctional).

--- Custom/Kernel/System/Ticket/Article.pm
+++ Custom/Kernel/System/Ticket/Article.pm
@@ -2,7 +2,9 @@
 # OTOBO is a web-based ticketing system for service organisations.
 # --
 # Copyright (C) 2001-2020 OTRS AG, https://otrs.com/
-# Copyright (C) 2019-2022 Rother OSS GmbH, https://otobo.de/
+# Copyright (C) 2019-2025 Rother OSS GmbH, https://otobo.io/
+# --
+# $origin: otobo - c4729719b9788da163909113055de495fac35df4 - Kernel/System/Ticket/Article.pm
 # --
 # This program is free software: you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
